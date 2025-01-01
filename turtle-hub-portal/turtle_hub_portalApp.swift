5bc84926074e9c4300b79529364964466defc3d7
Pavel Karpovich <pavel.karpovich@outlook.com>
2025-01-01 20:00:59 +0100

    Initial Commit

---
+++ turtle-hub-portal/turtle_hub_portalApp.swift
@@ -0,0 +1,17 @@
+//
+//  turtle_hub_portalApp.swift
+//  turtle-hub-portal
+//
+//  Created by Pavel Karpovich on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct turtle_hub_portalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
